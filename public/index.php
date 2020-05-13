110dbfd2c15dce06c72f21cf50d095bf345f4a45
Mohammad <hosseini.mohamad94@gmail.com>
2020-05-13 22:25:14 +0430

    Move application logic out of entry point

--- public/index.php
+++ public/index.php
@@ -1,16 +1,11 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require '../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
-});
+require '../src/app.php';
 
 $app->run();
