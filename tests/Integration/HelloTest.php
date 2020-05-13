80610037bf4e1cf22d801166bcde2fa07f4bb16d
Mohammad <hosseini.mohamad94@gmail.com>
2020-05-13 22:41:27 +0430

    Actual integration testing for Hello world app

--- tests/Integration/HelloTest.php
+++ tests/Integration/HelloTest.php
@@ -1,12 +1,24 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use Slim\Factory\AppFactory;
+use Slim\Psr7\Factory\ServerRequestFactory;
 
 class HelloTest extends TestCase
 {
     /** @test */
     public function it_says_hello()
     {
-        $this->assertEquals(1, 1);
+        $app = AppFactory::create();
+
+        require 'src/app.php';
+
+        $factory = new ServerRequestFactory();
+
+        $response = $app->handle($factory->createServerRequest('GET', '/'));
+
+        $this->assertEquals($response->getStatusCode(), 200);
+
+        $this->assertEquals((string) $response->getBody(), 'Hello world!');
     }
 }
