ac8d37f82c489f31ebcff45b5e459b3b1c3c746b
Mohammad <hosseini.mohamad94@gmail.com>
2020-05-24 10:37:46 +0430

    Setup monolog

--- public/index.php
+++ public/index.php
@@ -2,16 +2,21 @@
 
 use Slim\Factory\AppFactory;
 use League\Container\Container;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 require '../vendor/autoload.php';
 
 $container = new Container();
 
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('../app.log'));
+
 AppFactory::setContainer($container);
 $app = AppFactory::create();
 
 $app->addRoutingMiddleware();
-$app->addErrorMiddleware(true, false, false);
+$app->addErrorMiddleware(true, false, false, $log);
 
 require '../src/app.php';
 
