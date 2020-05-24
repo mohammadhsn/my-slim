fda28457d55bb81d65ae73b50630c99cd25c1656
Mohammad <hosseini.mohamad94@gmail.com>
2020-05-24 10:44:21 +0430

    Add settings file

--- public/index.php
+++ public/index.php
@@ -7,16 +7,23 @@ use Monolog\Handler\StreamHandler;
 
 require '../vendor/autoload.php';
 
+$settings = require '../src/settings.php';
+
 $container = new Container();
 
-$log = new Logger('name');
-$log->pushHandler(new StreamHandler('../app.log'));
+$log = new Logger($settings['app']['name']);
+$log->pushHandler(new StreamHandler("../{$settings['logging']['filename']}"));
 
 AppFactory::setContainer($container);
 $app = AppFactory::create();
 
 $app->addRoutingMiddleware();
-$app->addErrorMiddleware(true, false, false, $log);
+$app->addErrorMiddleware(
+    $settings['app']['debug'],
+    $settings['logging']['enable'],
+    $settings['logging']['detail'],
+    $log
+);
 
 require '../src/app.php';
 
---
+++ src/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'app' => [
+        'name'  => 'my-slim',
+        'debug' => true
+    ],
+    'logging' => [
+        'enable' => true,
+        'detail' => true,
+        'filename' => 'app.log'
+    ]
+];
