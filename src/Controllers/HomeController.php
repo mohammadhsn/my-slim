00946faacbb43be824dae236e36bfe454b89eb11
Mohammad <hosseini.mohamad94@gmail.com>
2020-05-24 14:07:46 +0430

    use Controller as request handler

---
+++ src/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Controllers;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+
+class HomeController
+{
+    public function home(Request $request, Response $response, $args)
+    {
+        $response->getBody()->write("Hello world!");
+        return $response;
+    }
+}
--- src/app.php
+++ src/app.php
@@ -1,11 +1,7 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+use App\Controllers\HomeController;
 
 /** @var Slim\App $app */
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
-});
+$app->get('/', HomeController::class . ':home');
