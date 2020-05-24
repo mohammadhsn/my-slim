54191da420b4974a93cbd464832efa389aed20c2
Mohammad <hosseini.mohamad94@gmail.com>
2020-05-24 15:53:34 +0430

    Add primitive data layer

---
+++ db/migrations/20200524111343_create_users_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUsersTable extends Migration
+{
+
+    public function up()
+    {
+        $this->schema->create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        $this->schema->drop('users');
+    }
+
+}
