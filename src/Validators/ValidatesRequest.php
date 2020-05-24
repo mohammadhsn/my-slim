06144fac1fd7c4529b5e3a99962c8fadc6c6bd49
Mohammad <hosseini.mohamad94@gmail.com>
2020-05-24 15:16:34 +0430

    Setup request validations

---
+++ src/Validators/ValidatesRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Validators;
+
+
+use Valitron\Validator;
+
+trait ValidatesRequest
+{
+    public function validate(?array $data, array $rules)
+    {
+        $validator = $this->getValidator($data ?: []);
+
+        $validator->rules($rules);
+
+        if (!$validator->validate()) {
+            throw new ValidatorException($validator->errors());
+        }
+
+        return $validator->data();
+    }
+
+    public function getValidator(array $data)
+    {
+        return new Validator($data);
+    }
+}
---
+++ src/Validators/ValidatorException.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Validators;
+
+
+use Exception;
+
+class ValidatorException extends Exception
+{
+    const CODE = 422;
+
+    protected $validator;
+
+    public function __construct(array $errors, $code = self::CODE)
+    {
+        parent::__construct(json_encode($errors), $code);
+    }
+}
