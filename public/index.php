3933bd8963bdd72a09ae5312bb67b7cc9d34fec7
Mohammad <hosseini.mohamad94@gmail.com>
2020-05-24 10:13:44 +0430

    Setup a psr-11 container

--- public/index.php
+++ public/index.php
@@ -1,9 +1,13 @@
 <?php
 
 use Slim\Factory\AppFactory;
+use League\Container\Container;
 
 require '../vendor/autoload.php';
 
+$container = new Container();
+
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 
 require '../src/app.php';
